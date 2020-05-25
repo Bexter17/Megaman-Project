b77c592a1fa846991ffd1916623ba748969df86d
Bexter <63883117+Bexter17@users.noreply.github.com>
2020-05-25 09:33:46 -0400

    Added MegaMan project

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public SpriteRenderer Megaman;
+
+    
+    public Transform spawnPointLeft;
+    public Transform spawnpoint;
+    public float projectileSpeed;
+    public Projectile projectilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (projectileSpeed <= 0)
+        {
+            projectileSpeed = 7.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shot();
+        }
+    }
+
+    void shot()
+    {
+        if (Megaman.flipX)
+        {
+           Projectile temp = Instantiate(projectilePrefab, spawnPointLeft.position, spawnPointLeft.rotation);
+            temp.speed = -projectileSpeed;
+        }
+        else
+        {
+             Instantiate(projectilePrefab, spawnpoint.position, spawnpoint.rotation);
+        }
+       // Projectile temp = Instantiate(projectilePrefab, spawnpoint.position, spawnpoint.rotation);
+
+       // temp.speed = -projectileSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody2D myRigidBody;
+    public int JumpForce;
+    public float speed;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public Transform groundCheck;
+    public LayerMask isGroundLayer;
+    public Bullet projectile;
+    public SpriteRenderer Megamansprite;
+
+
+    private bool isFacingLeft = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (JumpForce <= 0)
+        {
+            JumpForce = 5;
+        }
+
+       if (!groundCheck)
+        {
+            Debug.Log("Groundcheck is not found ");
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.2f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool Jump = Input.GetButtonDown("Jump");
+        bool Fire1 = Input.GetButtonDown("Fire1");
+        float moveValue = Input.GetAxisRaw("Horizontal");
+        anim.SetBool("Fire1", Fire1);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            
+            myRigidBody.velocity = Vector2.zero;
+
+            myRigidBody.AddForce(Vector2.up * JumpForce);
+
+
+        }
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        myRigidBody.velocity = new Vector2(horizontalInput * speed, myRigidBody.velocity.y);
+
+        anim.SetFloat("horizontalInput", Mathf.Abs(horizontalInput));
+
+        anim.SetBool("Jump", isGrounded);
+
+
+       
+        if(horizontalInput > 0 && isFacingLeft || horizontalInput < 0 && !isFacingLeft)
+        {
+            flip();
+        }
+
+        void flip()
+        {
+            isFacingLeft = !isFacingLeft;
+
+            Megamansprite.flipX = isFacingLeft;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire1 = true;
+            Debug.Log("FIRE");
+        }
+
+        else Fire1 = false;
+
+      
+
+        myRigidBody.velocity = new Vector2(moveValue * speed, myRigidBody.velocity.y);
+
+
+
+        
+
+
+
+    }
+}
